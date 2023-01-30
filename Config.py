7f40267d37ef27d7f3d3a63fd0d87354e98b08c0
Samy Chaabi <xhackax47@gmail.com>
2023-01-30 22:41:41 +0100

    Fonction de traitement des données + Fonction de création du modèle Deep Learning pour entraîner le chatbot

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import json
 import nltk
 import numpy as np
-import random 
+import random
 import string
-import tensorflow as tf 
+import tensorflow as tf
 import time
 
-from nltk.stem import WordNetLemmatizer 
-from tensorflow.keras import Sequential 
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from Intents import *
 
@@ -24,7 +24,7 @@ Created on Mon Jan 30 19:46:34 2023
 nltk.download('all')
 # initialisation de lemmatizer pour obtenir la racine des mots
 lemmatizer = WordNetLemmatizer()
-# création des listes
+"""Création des listes"""
 # vocabulaire de tous les mots utilisés dans les patterns
 mots = []
 # Liste des tags de chaque intention
@@ -33,20 +33,33 @@ classes = []
 doc_X = []
 # Liste des tags associés à chaque pattern dans le fichier des intentions
 doc_y = []
+# Listes pour les données d'entraînement
+entrainement = []
+entrainement_x = []
+entrainement_y = []
+# Modèle Deep Learning
+model = Sequential()
+shapes = []
 
 # Fonctions
 
+
 def programme():
+    """Programme principal"""
     print("INITIALISATION ET DEMARRAGE DU PROGRAMME DE CHATBOT PAR DEEP LEARNING")
     time.sleep(5)
     separationDonnees(mots, classes)
-    affichageDonnées(mots, classes)
+    shapes = traitementDonnees(entrainement, entrainement_x, entrainement_y)
+    creationModelChatbot(model, entrainement_x,
+                         entrainement_y, shapes[0], shapes[1])
     print("FIN DU PROGRAMME")
-    
+
+
 def separationDonnees(mots, classes):
     """Séparation des données et remplissage des listes (mots et classes) à partir des données des intentions"""
-    print("La séparation des données va bientôt commencée..")
-    time.sleep(3)
+    print("La séparation des données va bientôt commencer..")
+    time.sleep(2)
+
     # parcourir avec une boucle For toutes les intentions
     # tokéniser chaque pattern et ajouter les tokens à la liste words, les patterns et
     # le tag associé à l'intention sont ajoutés aux listes correspondantes
@@ -56,22 +69,87 @@ def separationDonnees(mots, classes):
             mots.extend(tokens)
             doc_X.append(pattern)
             doc_y.append(intent["tag"])
-        
-        # ajouter le tag aux classes s'il n'est pas déjà là 
+
+        # ajouter le tag aux classes s'il n'est pas déjà là
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
     # lemmatiser tous les mots du vocabulaire et les convertir en minuscule
     # si les mots n'apparaissent pas dans la ponctuation
-    mots = [lemmatizer.lemmatize(mot.lower()) for mot in mots if mot not in string.punctuation]
+    mots = [lemmatizer.lemmatize(mot.lower())
+            for mot in mots if mot not in string.punctuation]
     # trier le vocabulaire et les classes par ordre alphabétique et prendre le
     # set pour s'assurer qu'il n'y a pas de doublons
     mots = sorted(set(mots))
     classes = sorted(set(classes))
-
-def affichageDonnées(mots, classes):
-    print("Affichage des données : ")
-    time.sleep(3)
+    print("separationDonnees OK !!!")
+    print("Affichage des données brutes : ")
     print("Mots : " + str(mots))
     print("Classes : " + str(classes))
     print("Questions : " + str(doc_X))
     print("Type : " + str(doc_y))
+    time.sleep(3)
+
+
+def traitementDonnees(entrainement, entrainement_x, entrainement_y):
+    """Traitement des données séparées avec création des listes pour l'entraînement et conversion en valeurs numériques"""
+    print("Le traitement des données va bientôt commencer..")
+    time.sleep(2)
+
+    sortie_vide = [0] * len(classes)
+    # Création du modèle d'ensemble de mots
+    for idx, doc in enumerate(doc_X):
+        bow = []
+        texte = lemmatizer.lemmatize(doc.lower())
+        for mot in mots:
+            bow.append(1) if mot in texte else bow.append(0)
+        # Marque l'index de la classe à laquelle le pattern atguel est associé à
+        ligne_sortie = list(sortie_vide)
+        ligne_sortie[classes.index(doc_y[idx])] = 1
+        # Ajoute le one hot encoded BoW et les classes associées à la liste training
+        entrainement.append([bow, ligne_sortie])
+    # Mélanger les données et les convertir en array
+    random.shuffle(entrainement)
+    entrainement = np.array(entrainement, dtype=object)
+    # Séparer les features et les labels target
+    entrainement_x = np.array(list(entrainement[:, 0]))
+    entrainement_y = np.array(list(entrainement[:, 1]))
+    # Définition des paramètres
+    input_shape = (len(entrainement_x[0]),)
+    output_shape = len(entrainement_y[0])
+    shapes = [input_shape, output_shape]
+
+    print("traitementDonnees OK !!!")
+    print("Affichage des données au format numérique : ")
+    print("entrainement_x : " + str(entrainement_x))
+    print("entrainement_y : " + str(entrainement_y))
+    print("shapes : " + str(shapes))
+    time.sleep(3)
+    return shapes
+
+
+def creationModelChatbot(model, entrainement_x, entrainement_y, input_shape, output_shape):
+    """Création et définition du modèle de Deep Learning"""
+    print("La création du modèle Deep Learning va bientôt commencer..")
+    time.sleep(2)
+
+    #epochs = 200
+    # Définition du modèle Deep Learning
+    model.add(Dense(128, input_shape=input_shape, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(output_shape, activation="softmax"))
+    adam = tf.keras.optimizers.Adam(learning_rate=0.01, decay=1e-6)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=adam, metrics=["accuracy"])
+    print("creationModelChatbot OK !!!")
+    print("Affichage le sommaire du modèle Deep Learning : ")
+    print(model.summary())
+    """Entraînement du modèle de Deep Learning"""
+    print("L'entraînement du modèle Deep Learning va bientôt commencer..")
+    time.sleep(2)
+
+    # Entrainement du modèle
+    model.fit(x=entrainement_x, y=entrainement_y, epochs=200, verbose=1)
+    print("entrainementModelChatbot OK !!!")
+    time.sleep(3)
